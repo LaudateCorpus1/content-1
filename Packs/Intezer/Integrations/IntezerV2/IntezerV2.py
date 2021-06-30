8c9d64ecb7918d3bff0377cec29c57b667638b40
kfirstri <kfirstar@gmail.com>
2021-06-30 14:26:37 +0300

    Feat/intezer improvments (#1)
    * converted intezer integration to work with intezer sdk
    
    * added more commands
    
    * feat(intezer): added code-reuse command
    
    * new feature for intezer integration
    
    * Added Release notes and small fixes
    
    * rename integraiton changelog filename
    
    * remove redundant comments
    
    * bump intezer sdk version

--- Packs/Intezer/Integrations/IntezerV2/IntezerV2.py
+++ Packs/Intezer/Integrations/IntezerV2/IntezerV2.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 from typing import Callable
 from typing import Dict
 from typing import List
+from typing import Optional
 from typing import Union
 
 import demistomock as demisto
@@ -15,9 +16,7 @@ from intezer_sdk.analysis import get_latest_analysis
 from intezer_sdk.api import IntezerApi
 from intezer_sdk.errors import AnalysisIsAlreadyRunning
 from intezer_sdk.errors import AnalysisIsStillRunning
-from intezer_sdk.errors import FamilyNotFoundError
 from intezer_sdk.errors import HashDoesNotExistError
-from intezer_sdk.errors import InvalidApiKey
 from intezer_sdk.family import Family
 from intezer_sdk.sub_analysis import SubAnalysis
 from requests import HTTPError
@@ -27,6 +26,18 @@ from requests import HTTPError
 requests.packages.urllib3.disable_warnings()
 
 IS_AVAILABLE_URL = 'is-available'
+ERROR_PREFIX = 'Error from Intezer:'
+ACCEPTABLE_HTTP_CODES = {200, 201, 202}
+
+http_status_to_error_massage = {
+    400: '400 Bad Request - Wrong or invalid parameters',
+    401: '401 Unauthorized - Wrong or invalid api key',
+    403: '403 Forbidden - The account is not allowed to preform this task',
+    404: '404 Not Found - Analysis was not found',
+    410: '410 Gone - Analysis no longer exists in the service',
+    500: '500 Internal Server Error - Internal error',
+    503: '503 Service Unavailable'
+}
 
 dbot_score_by_verdict = {
     'malicious': 3,
@@ -55,14 +66,19 @@ def _get_missing_file_result(file_hash: str) -> CommandResults:
     )
 
 
-def _get_missing_analysis_result(analysis_id: str, sub_analysis_id: str = None) -> CommandResults:
-    if not sub_analysis_id:
-        output = f'The Analysis {analysis_id} was not found on Intezer Analyze'
-    else:
-        output = f'Could not find the analysis \'{analysis_id}\' or the sub analysis \'{sub_analysis_id}\''
+def _get_analysis_running_result(analysis_id: str = None, response: requests.Response = None) -> CommandResults:
+    analysis_id = analysis_id or response.json()['result_url'].split('/')[2]
+
+    context_json = {
+        'Intezer.Analysis(val.ID && val.ID == obj.ID)': {
+            'ID': analysis_id,
+            'Status': 'InProgress'
+        }
+    }
 
     return CommandResults(
-        readable_output=output
+        readable_output='Analysis is still in progress',
+        outputs=context_json
     )
 
 
@@ -72,44 +88,13 @@ def _get_missing_family_result(family_id: str) -> CommandResults:
     )
 
 
-def _get_analysis_running_result(analysis_id: str = None, response: requests.Response = None) -> CommandResults:
-    if response:
-        analysis_id = response.json()['result_url'].split('/')[2]
-
-    context_json = {
-        'ID': analysis_id,
-        'Status': 'InProgress'
-    }
-
-    return CommandResults(
-        outputs_prefix='Intezer.Analysis',
-        outputs_key_field='ID',
-        readable_output='Analysis is still in progress',
-        outputs=context_json
-    )
+def check_is_available(intezer_api: IntezerApi, args: Optional[dict]) -> str:
+    result = intezer_api.get_url_result(f'/{IS_AVAILABLE_URL}')
+    return 'ok' if result else 'Failure'
 
 
-''' COMMANDS '''
-
-
-def check_is_available(intezer_api: IntezerApi, args: dict) -> str:
-    try:
-        response = intezer_api.get_url_result(f'/{IS_AVAILABLE_URL}')
-        return 'ok' if response else 'Empty response from intezer service'
-    except InvalidApiKey as error:
-        return f'Invalid API key received.\n{error}'
-    except HTTPError as error:
-        return f'Error occurred when reaching Intezer Analyze. Please check Analyze Base URL. \n{error}'
-    except ConnectionError as error:
-        return f'Error connecting to Analyze Base url.\n{error}'
-
-
-def analyze_by_hash_command(intezer_api: IntezerApi, args: Dict[str, str]) -> CommandResults:
+def analyze_by_hash_command(intezer_api: IntezerApi, args: Optional[dict]) -> CommandResults:
     file_hash = args.get('file_hash')
-
-    if not file_hash:
-        raise ValueError('Missing file hash')
-
     analysis = Analysis(file_hash=file_hash, api=intezer_api)
 
     try:
@@ -117,28 +102,26 @@ def analyze_by_hash_command(intezer_api: IntezerApi, args: Dict[str, str]) -> Co
         analysis_id = analysis.analysis_id
 
         context_json = {
-            'ID': analysis.analysis_id,
-            'Status': 'Created',
-            'type': 'File'
+            'Intezer.Analysis(val.ID && val.ID == obj.ID)': {
+                'ID': analysis.analysis_id,
+                'Status': 'Created',
+                'type': 'File'
+            }
         }
 
         return CommandResults(
-            outputs_prefix='Intezer.Analysis',
-            outputs_key_field='ID',
             outputs=context_json,
             readable_output='Analysis created successfully: {}'.format(analysis_id)
         )
     except HashDoesNotExistError:
         return _get_missing_file_result(file_hash)
     except AnalysisIsAlreadyRunning as error:
-        return _get_analysis_running_result(response=error.response)
+        return _get_analysis_running_result(error.response)
 
+''' COMMANDS '''
 
-def get_latest_result_command(intezer_api: IntezerApi, args: Dict[str, str]) -> CommandResults:
+def get_latest_result_command(intezer_api: IntezerApi, args: Optional[dict]) -> CommandResults:
     file_hash = args.get('file_hash')
-
-    if not file_hash:
-        raise ValueError('Missing file hash')
 
     latest_analysis = get_latest_analysis(file_hash=file_hash, api=intezer_api)
 
@@ -148,7 +131,7 @@ def get_latest_result_command(intezer_api: IntezerApi, args: Dict[str, str]) -> 
     return enrich_dbot_and_display_file_analysis_results(latest_analysis.result())
 
 
-def analyze_by_uploaded_file_command(intezer_api: IntezerApi, args: dict) -> CommandResults:
+def analyze_by_uploaded_file_command(intezer_api: IntezerApi, args: Optional[dict]) -> CommandResults:
     file_id = args.get('file_entry_id')
     file_data = demisto.getFilePath(file_id)
 
@@ -157,103 +140,82 @@ def analyze_by_uploaded_file_command(intezer_api: IntezerApi, args: dict) -> Com
         analysis.send()
 
         context_json = {
-            'ID': analysis.analysis_id,
-            'Status': 'Created',
-            'type': 'File'
+            'Intezer.Analysis(val.ID && val.ID == obj.ID)': {
+                'ID': analysis.analysis_id,
+                'Status': 'Created',
+                'type': 'File'
+            }
         }
 
         return CommandResults(
-            outputs_prefix='Intezer.Analysis',
-            outputs_key_field='ID',
             outputs=context_json,
             readable_output='Analysis created successfully: {}'.format(analysis.analysis_id)
         )
     except AnalysisIsAlreadyRunning as error:
-        return _get_analysis_running_result(response=error.response)
+        return _get_analysis_running_result(error.response)
 
 
-def check_analysis_status_and_get_results_command(intezer_api: IntezerApi, args: dict) -> List[CommandResults]:
+def check_analysis_status_and_get_results_command(intezer_api: IntezerApi, args: Optional[dict]) -> CommandResults:
     analysis_type = args.get('analysis_type', 'File')
-    analysis_ids = argToList(args.get('analysis_id'))
+    analysis_id = args.get('analysis_id')
     indicator_name = args.get('indicator_name')
 
-    command_results = []
+    try:
+        if analysis_type == 'Endpoint':
+            response = intezer_api.get_url_result(f'/endpoint-analyses/{analysis_id}')
+            analysis_result = response.json()['result']
+        else:
+            analysis = get_analysis_by_id(analysis_id, api=intezer_api)
+            analysis_result = analysis.result()
 
-    for analysis_id in analysis_ids:
-        try:
-            if analysis_type == 'Endpoint':
-                response = intezer_api.get_url_result(f'/endpoint-analyses/{analysis_id}')
-                analysis_result = response.json()['result']
-            else:
-                analysis = get_analysis_by_id(analysis_id, api=intezer_api)
-                analysis_result = analysis.result()
+        if analysis_result and analysis_type == 'Endpoint':
+            return enrich_dbot_and_display_endpoint_analysis_results(analysis_result, indicator_name)
+        elif analysis_result and analysis_type == 'File':
+            return enrich_dbot_and_display_file_analysis_results(analysis_result)
 
-            if analysis_result and analysis_type == 'Endpoint':
-                command_results.append(
-                    enrich_dbot_and_display_endpoint_analysis_results(analysis_result, indicator_name))
-            else:
-                command_results.append(enrich_dbot_and_display_file_analysis_results(analysis_result))
-
-        except HTTPError as http_error:
-            if http_error.response.status_code == HTTPStatus.CONFLICT:
-                command_results.append(_get_analysis_running_result(analysis_id=analysis_id))
-            elif http_error.response.status_code == HTTPStatus.NOT_FOUND:
-                command_results.append(_get_missing_analysis_result(analysis_id))
-            else:
-                raise http_error
-        except AnalysisIsStillRunning:
-            command_results.append(_get_analysis_running_result(analysis_id=analysis_id))
-
-    return command_results
+    except HTTPError as http_error:
+        if http_error.response.status_code == HTTPStatus.CONFLICT:
+            return _get_analysis_running_result(analysis_id)
+        else:
+            raise http_error
+    except AnalysisIsStillRunning:
+        return _get_analysis_running_result(analysis_id=analysis_id)
 
 
-def get_analysis_sub_analyses_command(intezer_api: IntezerApi, args: dict) -> CommandResults:
+def get_analysis_sub_analyses_command(intezer_api: IntezerApi, args: Optional[dict]) -> CommandResults:
     analysis_id = args.get('analysis_id')
 
-    try:
-        analysis = get_analysis_by_id(analysis_id, api=intezer_api)
-    except HTTPError as error:
-        if error.response.status_code == HTTPStatus.NOT_FOUND:
-            return _get_missing_analysis_result(analysis_id=str(analysis_id))
-    except AnalysisIsStillRunning:
-        return _get_analysis_running_result(analysis_id=str(analysis_id))
-
+    analysis = get_analysis_by_id(analysis_id, api=intezer_api)
     sub_analyses: List[SubAnalysis] = analysis.get_sub_analyses()
 
     all_sub_analyses_ids = [sub.analysis_id for sub in sub_analyses]
     sub_analyses_table = tableToMarkdown('Sub Analyses', all_sub_analyses_ids, headers=['Analysis IDs'])
 
     context_json = {
-        'ID': analysis.analysis_id,
-        'SubAnalysesIDs': all_sub_analyses_ids
+        'Intezer.Analysis(val.ID && val.ID == obj.ID)': {
+            'ID': analysis.analysis_id,
+            'SubAnalyses': all_sub_analyses_ids
+        }
     }
 
     return CommandResults(
-        outputs_prefix='Intezer.Analysis',
-        outputs_key_field='ID',
         readable_output=sub_analyses_table,
         outputs=context_json,
         raw_response=all_sub_analyses_ids
     )
 
 
-def get_analysis_code_reuse_command(intezer_api: IntezerApi, args: dict) -> CommandResults:
+def get_analysis_code_reuse_command(intezer_api: IntezerApi, args: Optional[dict]) -> CommandResults:
     analysis_id = args.get('analysis_id')
     sub_analysis_id = args.get('sub_analysis_id', 'root')
 
-    try:
-        sub_analysis: SubAnalysis = SubAnalysis(analysis_id=sub_analysis_id,
-                                                composed_analysis_id=analysis_id,
-                                                sha256='',
-                                                source='',
-                                                api=intezer_api)
+    sub_analysis: SubAnalysis = SubAnalysis(analysis_id=sub_analysis_id,
+                                            composed_analysis_id=analysis_id,
+                                            sha256='',
+                                            source='',
+                                            api=intezer_api)
 
-        sub_analysis_code_reuse = sub_analysis.code_reuse
-    except HTTPError as error:
-        if error.response.status_code == HTTPStatus.NOT_FOUND:
-            return _get_missing_analysis_result(analysis_id=str(analysis_id))
-        elif error.response.status_code == HTTPStatus.CONFLICT:
-            return _get_analysis_running_result(analysis_id=str(analysis_id))
+    sub_analysis_code_reuse = sub_analysis.code_reuse
 
     if not sub_analysis_code_reuse:
         return CommandResults(
@@ -268,25 +230,14 @@ def get_analysis_code_reuse_command(intezer_api: IntezerApi, args: dict) -> Comm
         readable_output += '\nFamilies:\n'
         readable_output += '\n'.join(tableToMarkdown(family['family_name'], family) for family in families)
 
-    is_root = sub_analysis_id == 'root'
-
-    if is_root:
-        context_json = {
-            'Intezer.Analysis(obj.ID == val.ID)': {
-                'ID': analysis_id,
-                'CodeReuse': sub_analysis_code_reuse,
-                'CodeReuseFamilies': families
-            }
+    context_json = {
+        'Intezer.Analysis(val.ID && val.ID == obj.ID)': {
+            'ID': analysis_id if sub_analysis_id == 'root' else sub_analysis_id,
+            'ComposedAnalysisID': analysis_id,
+            'CodeReuse': sub_analysis_code_reuse,
+            'CodeReuseFamilies': families
         }
-    else:
-        context_json = {
-            'Intezer.Analysis(obj.RootAnalysis == val.ID).SubAnalyses(obj.ID == val.ID)': {
-                'ID': sub_analysis_id,
-                'RootAnalysis': analysis_id,
-                'CodeReuse': sub_analysis_code_reuse,
-                'CodeReuseFamilies': families
-            }
-        }
+    }
 
     return CommandResults(
         readable_output=readable_output,
@@ -294,44 +245,31 @@ def get_analysis_code_reuse_command(intezer_api: IntezerApi, args: dict) -> Comm
         raw_response=sub_analysis.code_reuse
     )
 
-
-def get_analysis_metadata_command(intezer_api: IntezerApi, args: dict) -> CommandResults:
+def get_analysis_code_reuse_command(intezer_api: IntezerApi, args: dict) -> CommandResults:
     analysis_id = args.get('analysis_id')
     sub_analysis_id = args.get('sub_analysis_id', 'root')
 
-    try:
-        sub_analysis: SubAnalysis = SubAnalysis(analysis_id=sub_analysis_id,
-                                                composed_analysis_id=analysis_id,
-                                                sha256='',
-                                                source='',
-                                                api=intezer_api)
+def get_analysis_metadata_command(intezer_api: IntezerApi, args: Optional[dict]) -> CommandResults:
+    analysis_id = args.get('analysis_id')
+    sub_analysis_id = args.get('sub_analysis_id', 'root')
 
-        sub_analysis_metadata = sub_analysis.metadata
-    except HTTPError as error:
-        if error.response.status_code == HTTPStatus.NOT_FOUND:
-            return _get_missing_analysis_result(analysis_id=str(analysis_id))
-        elif error.response.status_code == HTTPStatus.CONFLICT:
-            return _get_analysis_running_result(analysis_id=str(analysis_id))
+    sub_analysis: SubAnalysis = SubAnalysis(analysis_id=sub_analysis_id,
+                                            composed_analysis_id=analysis_id,
+                                            sha256='',
+                                            source='',
+                                            api=intezer_api)
+
+    sub_analysis_metadata = sub_analysis.metadata
 
     metadata_table = tableToMarkdown('Analysis Metadata', sub_analysis_metadata)
 
-    is_root = sub_analysis_id == 'root'
-
-    if is_root:
-        context_json = {
-            'Intezer.Analysis(obj.ID == val.ID)': {
-                'ID': analysis_id,
-                'Metadata': sub_analysis_metadata
-            }
+    context_json = {
+        'Intezer.Analysis(val.ID && val.ID == obj.ID)': {
+            'ID': analysis_id if sub_analysis_id == 'root' else sub_analysis_id,
+            'ComposedAnalysisID': analysis_id,
+            'Metadata': sub_analysis_metadata
         }
-    else:
-        context_json = {
-            'Intezer.Analysis(obj.RootAnalysis == val.ID).SubAnalyses(obj.ID == val.ID)': {
-                'ID': sub_analysis_id,
-                'RootAnalysis': analysis_id,
-                'Metadata': sub_analysis_metadata
-            }
-        }
+    }
 
     return CommandResults(
         readable_output=metadata_table,
@@ -339,15 +277,16 @@ def get_analysis_metadata_command(intezer_api: IntezerApi, args: dict) -> Comman
         raw_response=sub_analysis_metadata
     )
 
+    return CommandResults(
+        readable_output=metadata_table,
+        outputs=context_json,
+        raw_response=sub_analysis_metadata
+    )
 
-def get_family_info_command(intezer_api: IntezerApi, args: dict) -> CommandResults:
+def get_family_info_command(intezer_api: IntezerApi, args: Optional[dict]) -> CommandResults:
     family_id = args.get('family_id')
     family = Family(family_id, api=intezer_api)
-
-    try:
-        family.fetch_info()
-    except FamilyNotFoundError:
-        return _get_missing_family_result(str(family_id))
+    family.fetch_info()
 
     output = {
         'ID': family_id,
@@ -450,12 +389,11 @@ def main():
 
         intezer_api_key = demisto.getParam('APIKey')
         intezer_base_url_param = demisto.getParam('AnalyzeBaseURL')
-        use_ssl = not demisto.params().get('insecure', False)
         analyze_base_url = intezer_base_url_param or consts.BASE_URL
 
-        intezer_api = IntezerApi(consts.API_VERSION, intezer_api_key, analyze_base_url, use_ssl)
+        intezer_api = IntezerApi(consts.API_VERSION, intezer_api_key, analyze_base_url)
 
-        command_handlers: Dict[str, Callable[[IntezerApi, dict], Union[List[CommandResults], CommandResults, str]]] = {
+        command_handlers: Dict[str, Callable[[IntezerApi, dict], Union[CommandResults, str]]] = {
             'test-module': check_is_available,
             'intezer-analyze-by-hash': analyze_by_hash_command,
             'intezer-analyze-by-file': analyze_by_uploaded_file_command,
@@ -476,6 +414,6 @@ def main():
         return_error(f'Failed to execute {command} command. Error: {str(e)}')
 
 
-# python2 uses __builtin__ python3 uses builtins
+# python2 uses __builtin__ python3 uses builtins yo
 if __name__ == "__builtin__" or __name__ == "builtins":
     main()
